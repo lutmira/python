6e8f8b3c3c116afe73642836cc1f14744ca10689
lutmira <73383728+lutmira@users.noreply.github.com>
2020-12-06 10:30:21 +0100

    Add files via upload

---
+++ findsqrt.py
@@ -0,0 +1,25 @@
+# PRSH problem
+# Finding the square root of no 5
+
+from random import random, randrange
+
+import math
+
+def FindSqrt(nr):
+    g=round(nr/randrange(1,nr))
+    while nr!=g*g:
+        print(g*g, (g+nr/g)/2)
+        if(nr==math.ceil(g*g)):
+            return g
+        else:
+            g=(g+nr/g)/2
+
+FindSqrt(8)
+
+
+
+
+
+
+
+
